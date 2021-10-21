133a22c08a15164c65219084165a7e295970db42
robbyxp1 <robxp@hotmail.com>
2021-10-21 12:40:23 +0100

    Add pack.bat

--- OFC/pack.bat
+++ OFC/pack.bat
@@ -1,1 +1,11 @@
+rem pack removes all bin/obj, removes previous nupkg and .zip, then rebuilds
+rem creates a .zip copy so you can check it manually - important
+
+del -confirm *.nupkg *.zip
+del -confirm /rp bin
+del -confirm /rp obj
+
 nuget pack OFC.csproj -Verbosity detailed -Build -Properties Configuration=Release
+
+copy *.nupkg *.zip
+
